b2c15140699c58cad59e22d05a3546f57441ad2d
Astronaut Group <19156046+alexanderjbeal@users.noreply.github.com>
2018-08-24 00:00:36 -0500

    Encrypted Login + Project File Structure

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
-require("dotenv").config();
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+const express = require('express');
+const app = express();
+let passport = require('passport');
+let session = require('express-session');
+let bodyParser = require('body-parser');
+let env = require('dotenv').load();
+let exphbs = require('express-handlebars');
 
-var db = require("./models");
+let PORT = process.env.PORT || 5000;
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+// BodyParser (Middleware)
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+// Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+app.get('/', function(req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});
+
+
+// Models
+let models = require("./app/models");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+let authRoute = require('./app/routes/auth.js')(app,passport);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+// Load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+ 
+let syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -34,15 +50,36 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Start Server - Sync Database & Models
+models.sequelize.sync(syncOptions).then(function() {
+    app.listen(PORT, function(err) {
+        if (!err)
+        console.log(`
+        \nSite is live 🌎
+        \nServer running on ${PORT}
+        \nDatabase looks good!
+        `);
+        else console.log(err)
+      });
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the database update!")
 });
 
 module.exports = app;
+
+
+// var db = require("./models");
+
+// // Handlebars
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
+
+
+
+
+
